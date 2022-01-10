3feef2099132718e465802ce44cd8816d224ae0e
Haejin Lee <haejlee02@gmail.com>
2022-01-10 10:13:00 -0800

    Update AVLNode methods

--- src/AVLNode.java
+++ src/AVLNode.java
@@ -16,10 +16,31 @@ public class AVLNode<T> extends BSTNode<T> {
 	 * @param right
 	 * @param height
 	 */
-	public AVLNode(T data, BSTNode<T> left, BSTNode<T> right, int height) {
-		super(data, left, right);
-		this.height = height;
+	public AVLNode(T data) {
+		super(data);
+		height = 1;
 	}
+
+	public AVLNode<T> getLeft(){
+		return (AVLNode<T>)super.getLeft();
+	}
+
+	public void setLeft(AVLNode<T> left) {
+		super.setLeft(left);
+	}
+
+	public AVLNode<T> getRight() {
+		return (AVLNode<T>)super.getRight();
+	}
+
+	public void setRight(AVLNode<T> right) {
+		super.setRight(right);
+	}
+	
+	public AVLNode<T> getParent(){
+		return (AVLNode<T>)super.getParent();
+	}
+	
 	/**
 	 * @return height of the AVL tree
 	 */
@@ -27,5 +48,8 @@ public class AVLNode<T> extends BSTNode<T> {
 		return this.height;
 	}
 	
+	public void setHeight(int h) {
+		this.height = h;
+	}
 
 }
