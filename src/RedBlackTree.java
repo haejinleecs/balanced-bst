ed0f5b58b5e9ad7a744c873ba0909c7c0cfd81e3
Haejin Lee <haejlee02@gmail.com>
2022-01-22 18:16:56 -0800

    Add comments

--- src/RedBlackTree.java
+++ src/RedBlackTree.java
@@ -1,4 +1,16 @@
-
+/**
+ * Rules That Every Red-Black Tree Follows: 
+ * 
+ * 	1) Every node has a colour either red or black.
+ * 	2) The root of the tree is always black.
+ * 	3) There are no two adjacent red nodes (A red node cannot have a red parent or red child).
+ * 	4) Every path from a node (including root) to any of its descendants NULL nodes has the same number of black nodes.
+ * 	5) All leaf nodes are black nodes.
+ * 
+ * @author haejinlee
+ * 
+ * @param <T>
+ */
 public class RedBlackTree <T extends Comparable<T>> extends BinarySearchTree<T>{
 	
 	public RedBlackTree() {
